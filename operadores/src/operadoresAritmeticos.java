0036a44d43f61a8a88a266b933de600ed23abef3
Analécia Mariana <marianaanalecia@gmail.com>
2023-09-24 21:09:59 -0300

    atualização de aprendizagem

---
+++ operadores/src/operadoresAritmeticos.java
@@ -0,0 +1,11 @@
+public class operadoresAritmeticos {
+    //classe Operadores.java - "Aritméticos" representado pelos símbolos + (adição), - (subtração), * (multiplicação) e / (divisão).
+double soma = 10.5 + 15.7;
+int subtração = 113 - 25;
+int multiplicacao = 20 * 7;
+int divisao = 15 / 3;
+int modulo = 18 % 3;
+double resultado = (10 * 7) + (20/4);
+}
+
+// o operador de adição (+), quando utilizado em variáveis do tipo texto, realizará a “concatenação de textos”.
---
+++ operadores/src/operadoresAtribuicao.java
@@ -0,0 +1,11 @@
+public class operadoresAtribuicao {
+    public static void main(String[] args) throws Exception {
+        //classe Operadores.java - "Atribuição" que é representado pelo símbolo de igualdade
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date(); // o valor "new" quer dizer que estamos criando um novo objeto na aplicação
+    }
+}
